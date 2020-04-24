d768c6618229d5aff97f0c3f5a32ff24ade8c000
rizki <kikirizmau140175@gmail.com>
2020-04-24 07:40:41 +0700

    add animation

--- lib/screens/list_page.dart
+++ lib/screens/list_page.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:octodex_gallery/models/octodex.dart';
@@ -18,26 +19,37 @@ class _ListPageState extends State<ListPage> {
         title: Text('Octodex'),
       ),
       body: SafeArea(
-        child: StaggeredGridView.countBuilder(
-          padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
-          itemCount: data.length,
-          crossAxisCount: 4,
-          crossAxisSpacing: 4.0,
-          mainAxisSpacing: 4.0,
-          staggeredTileBuilder: (index) =>
-              StaggeredTile.count(2, index.isEven ? 1 : 2),
-          itemBuilder: (context, index) {
-            var name = data[index]['name'];
-            var url = data[index]['url'];
-            var author = data[index]['author'];
+        child: AnimationLimiter(
+          child: StaggeredGridView.countBuilder(
+            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+            itemCount: data.length,
+            crossAxisCount: 4,
+            crossAxisSpacing: 4.0,
+            mainAxisSpacing: 4.0,
+            staggeredTileBuilder: (index) =>
+                StaggeredTile.count(2, index.isEven ? 1 : 2),
+            itemBuilder: (context, index) {
+              var name = data[index]['name'];
+              var url = data[index]['url'];
+              var author = data[index]['author'];
 
-            return Card(
-              child: CachedNetworkImage(
-                imageUrl: url,
-                fit: BoxFit.cover,
-              ),
-            );
-          },
+              return AnimationConfiguration.staggeredGrid(
+                columnCount: data.length,
+                position: index,
+                duration: Duration(milliseconds: 375),
+                child: ScaleAnimation(
+                  child: FadeInAnimation(
+                    child: Card(
+                      child: CachedNetworkImage(
+                        imageUrl: url,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
